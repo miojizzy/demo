0a77fc0a3b9db4f6cd0a47391072093cad4e5b6c
miojizzy <908678961@qq.com>
2023-11-22 08:52:19 +0000

    update

---
+++ README.md
@@ -0,0 +1,23 @@
+# readme
+
+这里是各种例子的合集，速查速用
+
+
+## 目录内容
+
+- c++
+    - crip -> 静态多态
+    - enum_bitwise -> 使用enum class之后无法进行数值位运算的解决方案
+    - scope_guard -> 类似golang的defer的工具
+
+- compile
+    - make -> makefile的使用
+
+- python 
+    - command_line_opiton -> 命令行调用时的参数解析例子
+
+- bash
+    - oop -> 面向对象的封装
+    - command_line_opiton -> 命令行参数解析的简单封装
+
+
--- c++/scope_guard/scope_guard2.h
+++ c++/scope_guard/scope_guard2.h
@@ -3,5 +3,34 @@
 namespace sg2 {
 
 
+class ScopeGuardImplBase { // 基类
+public:
+    ScopeGuardImplBase() : commit_(false) {}
+    void commit() const noexcept { commit_ = true; } // commit提交方法，const的，并且noexcept
+protected:
+    ScopeGuardImplBase(const ScopeGuardImplBase &other) : commit_(other.commit_) { other.commit(); } // 拷贝构造函数
+    ~ScopeGuardImplBase() {}
+    mutable bool commit_; // mutable的
+private:
+    ScopeGuardImplBase &operator=(const ScopeGuardImplBase &) = delete; // 不允许赋值构造
+};
+
+
+template<typename Func, typename Arg>
+class ScopeGuardImpl : public ScopeGuardImplBase { // 派生实现类
+public:
+    ScopeGuardImpl(const Func &func, Arg &arg) : func_(func), arg_(arg) {}
+    ~ScopeGuardImpl() { if (!commit_) func_(arg_); } // 析构类
+private:
+    const Func &func_; // 方法成员变量，传入回滚方法。const
+    Arg &arg_;
+};
+
+
+template<typename Func, typename Arg>
+ScopeGuardImpl<Func, Arg> makeScopeGuard(const Func &func, Arg &arg) {
+    return ScopeGuardImpl<Func, Arg>(func, arg);
+
+}
 
 } // namespace sg2
